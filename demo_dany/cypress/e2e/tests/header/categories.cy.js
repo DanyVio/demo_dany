a18b4b8a87c149edfd52bae38feb9e548eb6851f
dany <danielv.magdas@gmail.com>
2025-01-27 12:43:27 +0100

    using copilot

--- demo_dany/cypress/e2e/tests/header/categories.cy.js
+++ demo_dany/cypress/e2e/tests/header/categories.cy.js
@@ -14,4 +14,21 @@ describe("Header", function () {
       .should('be.visible')
       .click();
   });
+  it("Check all categories in the header", function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('#hs-eu-confirmation-button').should('be.visible').click();
+    cy.get('#int-button-language').should('be.visible').click();
+    cy.get('.main-nav__list-item--dropdown > .main-nav__list-item-link--lvl1')
+      .trigger('mouseover')
+      .should('be.visible');
+
+    cy.get('.main-nav__list-item--dropdown > .main-nav__list-item-link--lvl1').each(($el, index, $list) => {
+      cy.wrap($el).click();
+      cy.url().should('include', $el.attr('href'));
+      cy.go('back');
+      cy.get('.main-nav__list-item--dropdown > .main-nav__list-item-link--lvl1')
+        .trigger('mouseover')
+        .should('be.visible');
+    });
+  });
 });
