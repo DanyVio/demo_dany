c643bfaea2cb487a48d8c84c4d00dd0574fe6c1a
dany <danielv.magdas@gmail.com>
2025-01-29 19:16:57 +0100

    fixing lines

--- demo_dany/cypress/e2e/tests/home-page/home-page.cy.js
+++ demo_dany/cypress/e2e/tests/home-page/home-page.cy.js
@@ -4,6 +4,7 @@ Cypress.on("uncaught:exception", (err, runnable) => {
   return false;
 });
 
+
 describe("Home Page", function() {
   it("Check the Home button", function () {
     cy.get('.main-nav__list-item-link--home').should('be.visible').click();
